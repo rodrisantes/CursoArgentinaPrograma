abed2db66d40e5a7ede8944facee55cbbbfe70ea
rodrisantes <104109457+rodrisantes@users.noreply.github.com>
2023-02-28 20:08:04 -0300

    Carpeta de Clases

--- Clase 1/Clase1.java
+++ Clase 1/Clase1.java
@@ -4,24 +4,29 @@ class Clase1 {
         int b = 20;
         int a = 10;
 
-        
         // for (int i = 0; i < b ; i++) {
-        //     while (i < b && a < i) {
-        //         System.out.println(i);
-        //         i++;
-        //     }
+        // while (i < b && a < i) {
+        // System.out.println(i);
+        // i++;
         // }
-        for (int i = 0; i < b ; i++) {
-            while (i < b && a < i && i % 2 == 0) {
+        // }
+        // for (int i = 0; i < b ; i++) {
+        // while (i < b && a < i && i % 2 == 0) {
+        // System.out.println(i);
+        // i++;
+        // }
+        // }
+        int c = 2;
+        for (int i = 0; i < b; i++) {
+            while (i < b && a < i && i % c == 0) {
                 System.out.println(i);
                 i++;
             }
         }
 
 
+        
 
     }
-
-    
 
 }
