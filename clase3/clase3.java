8682839a227be0f3e10b28a07822ef1ceec9d5e4
rodrisantes <104109457+rodrisantes@users.noreply.github.com>
2023-03-12 14:09:36 -0300

    Clase 3 completada

--- clase3/clase3.java
+++ clase3/clase3.java
@@ -1,5 +1,7 @@
 package Clase3;
 
+import java.util.Arrays;
+
 public class clase3 {
 
     public static class Ejercicio1A {
@@ -14,26 +16,83 @@ public class clase3 {
             }
             return contador;
         }
-      }
+    }
 
-    public static class Ejercicio2 {
-        public static int[] ordenar(int numero1, int numero2, int numero3) {
+    public static class Ejercicio1B {
+        public static int[] ordenar(int numero1, int numero2, int numero3, String orden) {
             int[] numeros = { numero1, numero2, numero3 };
 
+            if (orden.equals("ascendente")) {
+                Arrays.sort(numeros);
+            } else if (orden.equals("descendente")) {
+                Arrays.sort(numeros);
+                int numeroFinal = numeros[0];
+                numeros[0] = numeros[2];
+                numeros[2] = numeroFinal;
+            }
             return numeros;
 
         }
+    }
 
-        public static void main(String[] args) {
-            int[] numero = Ejercicio2.ordenar(5, 4, 4);
+    public static class Ejercicio1C {
 
-            for (int i = 0; i < numero.length; i++) {
-                System.out.print(numero[i] + " ");
+        public static int sumar(int[] array, int numero) {
+            int numerosSumados = 0;
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] > numero) {
+                    numerosSumados += array[i];
+                }
             }
-            System.out.println();
+            return numerosSumados;
+        }
+    }
+
+    public static class Ejercicio2 {
+
+        public static String codificacion(String cadena, int cantidadDeDesplazamientos) {
+            String palabraFinal = "";
+            String abecedario = "abcdefghijklmnñopqrstuvwxyz";
+            String[] abcededarioSplit = abecedario.split("");
+            String[] cadenaConEspacios = cadena.split(" ");
+            String cadenaSinEspacios = "";
+            for (int i = 0; i < cadenaConEspacios.length; i++) {
+                cadenaSinEspacios += cadenaConEspacios[i];
+            }
+            String[] cadenaSinEspaciosSeparada = cadenaSinEspacios.split("");
+
+            for (int i = 0; i < cadenaSinEspaciosSeparada.length; i++) {
+                for (int j = 0; j < abcededarioSplit.length; j++) {
+                    if (cadenaSinEspaciosSeparada[i].toLowerCase().equals(abcededarioSplit[j])) {
+
+                        palabraFinal += abcededarioSplit[j + cantidadDeDesplazamientos];
+                    }
+
+                }
+
+            }
+
+            return palabraFinal;
+
         }
 
     }
 
-}
+    public static void main(String[] args) {
 
+        // int[] numerosOrdenados = Ejercicio1B.ordenar(9, 4, 5, "ascendente");
+        // System.out.println(Arrays.toString(numerosOrdenados));
+
+        // int[] numeros = { 1, 2, 3, 4, 5 };
+        // int x = 2;
+        // int resultado = Ejercicio1C.sumar(numeros, x);
+        // System.out.println(resultado);
+
+        // String cadenaSinEspacios2 = Ejercicio2.codificacion("Los ratones mas bellos
+        // del universo", 1);
+        // System.out.println(cadenaSinEspacios2);
+
+    }
+
+}
---
+++ prueba.java
@@ -0,0 +1,19 @@
+public class prueba {
+
+    public static void main(String[] args) {
+        class Persona {
+            private String nombre;
+            private int edad;
+
+        }
+
+        Persona Rodrigo = new Persona();
+        Rodrigo.edad = 6;
+        Rodrigo.nombre = "Rodrigoasd";
+        System.out.println(Rodrigo.edad);
+
+        System.out.println(Rodrigo.nombre);
+
+    }
+
+}
